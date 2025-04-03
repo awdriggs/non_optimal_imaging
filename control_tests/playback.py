5c3003e07073b120dc96898c709dc9e41d12506f
awd <awdriggs@gmail.com>
2025-04-03 16:26:15 +0100

    playback with forward nav

--- control_tests/playback.py
+++ control_tests/playback.py
@@ -9,6 +9,7 @@ import board
 import digitalio
 import adafruit_rgb_display.st7789 as st7789
 from threading import Lock
+from pathlib import Path
 
 # === TFT Setup ===
 spi = board.SPI()
@@ -47,34 +48,9 @@ time.sleep(1)
 button = Button(14, bounce_time=0.1)
 toggle_button = Button(16, bounce_time=0.1)
 playback_button = Button(21, bounce_time=0.1)
+forward_button = Button(20, bounce_time=0.1)
 
 camera_lock = Lock()
-
-# === Display Shit === 
-display_mode = "off"
-# Backlight control
-backlight = digitalio.DigitalInOut(board.D18)
-backlight.direction = digitalio.Direction.OUTPUT
-backlight.value = False  # Backlight ON
-
-# === Preview Loop in a Background Thread ===
-def update_display():
-    while True:
-        if display_mode == "preview":
-            with camera_lock:
-                frame = picam2.capture_array("main")
-            image = Image.fromarray(frame, 'RGB')
-        elif display_mode == "playback":
-            image = Image.open("blinka.jpg") 
-        else:
-            image = blank_image
-
-        image = image.resize((width, height))  
-        disp.image(image)
-        time.sleep(0.03)  # ~30fps
-
-# Start the preview loop in a thread
-threading.Thread(target=update_display, daemon=True).start()
 
 # === Image Capture Function ===
 def capture_image():
@@ -92,6 +68,40 @@ def capture_image():
         picam2.stop()
         picam2.configure(preview_config)
         picam2.start()
+
+# === Display Shit === 
+display_mode = "off"
+# Backlight control
+backlight = digitalio.DigitalInOut(board.D18)
+backlight.direction = digitalio.Direction.OUTPUT
+backlight.value = False  # Backlight ON
+
+# === Preview Loop in a Background Thread ===
+def update_display():
+    while True:
+        if display_mode == "preview":
+            with camera_lock:
+                frame = picam2.capture_array("main")
+            image = Image.fromarray(frame, 'RGB')
+        elif display_mode == "playback":
+            images = get_images()
+
+            if get_images: #has at least one image
+                image = Image.open(path / images[playback_index])
+            else: 
+                print("no images")
+                image = blank_image
+
+            # image = Image.open("blinka.jpg") 
+        else:
+            image = blank_image
+
+        image = image.resize((width, height))  
+        disp.image(image)
+        time.sleep(0.03)  # ~30fps
+
+# Start the preview loop in a thread
+threading.Thread(target=update_display, daemon=True).start()
 
 def update_backlight():
     backlight.value = (display_mode != "off")
@@ -112,12 +122,36 @@ def handle_capture_button():
     else:
         capture_image()
 
+num_images = 0
+playback_index = 0
+
+# playback shit
+path = Path("/home/awdriggs/Pictures")
+
+def get_images():
+    image_list = [f.name for f in path.iterdir() if f.is_file()]
+    global num_images
+    num_images = len(image_list)
+    return image_list 
+
+def navigate(direction):
+    global playback_index
+    if direction == "forward":
+        playback_index += 1
+
+        if playback_index > num_images - 1:
+            playback_index = 0
+
+        print(f"{playback_index + 1} / {num_images}")
+        
+
 def test_button():
     print("button working");
 
 button.when_pressed = handle_capture_button 
 toggle_button.when_pressed = lambda: set_display_mode("preview")
 playback_button.when_pressed = lambda: set_display_mode("playback")
+forward_button.when_pressed = lambda: navigate("forward")
 
 pause()
 
