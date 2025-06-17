a5441e3c5cd5de701b662255ba53cce639419f63
awd <awdriggs@gmail.com>
2025-06-17 17:19:12 +0100

    hotfix status led

--- camera/capture.py
+++ camera/capture.py
@@ -69,7 +69,7 @@ def capture_image(camera, camera_lock, display):
 
         except Exception as e:
             print(f"Capture failed: {e}")
-            satus_led.off()
+            status_led.off()
             return None
    
         status_led.off()
